54aef20a5ca9e5b2508ae6ca6ea429e0d3f6738a
Zoggy <zoggy@bat8.org>
2016-12-14 16:02:56 +0100

    test-curl.sh script add  to command

--- src/test-curl.sh
+++ src/test-curl.sh
@@ -1,1 +1,1 @@
-./solid_ls -p me --curl https://zoggy.databox.me/Preferences/ https://zoggy.databox.me/Public/ https://zoggy.databox.me/Private/
+./solid_ls -p me --curl https://zoggy.databox.me/Preferences/ https://zoggy.databox.me/Public/ https://zoggy.databox.me/Private/ $*
